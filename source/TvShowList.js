c32a6791cfd543b65496c466d15a415649387736
John Kristensen <john@jerrykan.com>
2011-10-22 15:12:59 +1100

    Pretty up the TV Show listing
     * display tv show banner if available
     * show a tick if the show has been watched

--- source/TvShowList.js
+++ source/TvShowList.js
@@ -1,7 +1,54 @@
 enyo.kind({
     name: "Remote.TvShowList",
     kind: "Remote.ListView",
-    
     // Use the default updateItems()
     xbmcMethod: "getTvShows",
+    
+    components: [
+        {name: "items", kind: enyo.VirtualList, flex: 1, onSetupRow: "setupRow", components: [
+            // Row contents
+            {kind: "Item", layoutKind: enyo.HFlexLayout, height: "93px", pack: "center",
+             align: "center", style: "padding: 0;", onclick: "selectItem", components: [
+                // Container for the title, banner, and tick
+                {width: "484px", height: "89px", style: "position: relative;", components: [
+                    // Title - hidden behind the banner image
+                    {name: "title", width: "100%",
+                     style: "position: absolute; font-size: 21px; top: 34px; text-align: center; z-index: -1;"
+                    },
+                    // Banner Image
+                    {name: "banner", kind: enyo.Image, width: "100%", height: "100%",
+                     onerror: "errorImage"
+                    },
+                    // Watched Tick
+                    {name: "watched", kind: enyo.Image, src: "images/check.png", width: "40px",
+                     height: "40px", style: "position: absolute; bottom: 0; right: 0; z-index: 99"
+                    }
+                ]}
+            ]}
+        ]}
+    ],
+    
+    create: function() {
+        this.inherited(arguments);
+    },
+    setupRowItem: function(inIndex) {
+        this.$.title.setContent(this.items[inIndex].title);
+        
+        // Show a tick if all episodes have been watched
+        if (this.items[inIndex].played > 0) {
+            this.$.watched.show();
+        } else {
+            this.$.watched.hide();
+        }
+        
+        // Hide the banner if there is no image URL to use (the "hidden" title
+        // text will show through instead)
+        if (this.items[inIndex].banner !== "") {
+            this.$.banner.setSrc(this.items[inIndex].banner);
+        } else {
+            this.$.banner.hide();
+        }
+        
+        return true;
+    }
 });
