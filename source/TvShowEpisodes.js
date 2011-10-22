61d0bc39247d3c2603132fe8b2583e6713de3109
John Kristensen <john@jerrykan.com>
2011-10-23 00:09:05 +1100

    Pretty up the TV Show Episode listing
     * display a screenshot of the show, the episode number, title, and plot
       summary
     * show a tick if the show has been watched
     * show a rating out of 5 starts

--- source/TvShowEpisodes.js
+++ source/TvShowEpisodes.js
@@ -6,10 +6,59 @@ enyo.kind({
         season: null,
     },
     
+    components: [
+        {name: "items", kind: enyo.VirtualList, flex: 1, onSetupRow: "setupRow", components: [
+            // Row Item
+            {kind: "Item", layoutKind: enyo.HFlexLayout, height: "93px", pack: "center",
+             style: "padding: 0", onclick: "selectItem", components: [
+                // Screenshot, Ratings, Watched
+                {layoutKind: enyo.VFlexLayout, width: "119px", height: "89px", pack: "center",
+                 style: "margin-left: 5px; position: relative", components: [
+                    {name: "screenshot", kind: enyo.Image, width: "auto", height: "auto",
+                     style: "max-width: 100%; max-height: 100%;" 
+                    },
+                    {name: "watched", kind: enyo.Image, src: "images/check.png", width: "32px",
+                     height: "32px", style: "position: absolute; bottom: 0; right: 0;"
+                    },
+                    {name: "rating", kind: enyo.Image, src: "images/5star.png", width: "62px",
+                     height: "16px", style: "position: absolute; bottom: 0; left: 0;"
+                    }
+                ]},
+                // Title, Plot
+                {layoutKind: enyo.VFlexLayout, height: "100%", flex: 1, style: "padding: 5px;",
+                 components: [
+                    {layoutKind: enyo.HFlexLayout, style: "font-weight: bold", components: [
+                        {name: "episode"},
+                        {name: "title", style: "margin-left: 10px;"}
+                    ]},
+                    {name: "plot", height: "100%", style: "font-size: 12px; overflow: hidden;"}
+                ]}
+            ]}
+        ]}
+    ],
+    
+    setupRowItem: function(inIndex) {
+        var rating = this.items[inIndex].rating / 10 * parseInt(this.$.rating.width);
+        
+        this.$.screenshot.setSrc(this.items[inIndex].screenshot);
+        this.$.episode.setContent(this.items[inIndex].episode + ".");
+        this.$.title.setContent(this.items[inIndex].title);
+        this.$.plot.setContent(this.items[inIndex].plot);
+        this.$.rating.applyStyle("clip", "rect(0, " + rating + "px, " + this.$.rating.height+ ", 0)");
+        
+        if (this.items[inIndex].played > 0) {
+            this.$.watched.show();
+        } else {
+            this.$.watched.hide();
+        }
+        
+        return true;
+    },
+    
     updateItems: function() {
         this.requestItems("getTvShowEpisodes", {
             tvShowId: this.tvShowId,
             season: this.season,
         });
-    },
+    }
 });
